fac152a43274876bfa3909ca35c8e6f53b37b47d
ferdi ardiansa <ferdiardiansa@rocketmail.com>
2017-10-10 11:29:15 +0700

    make postEmbed factory

---
+++ database/factories/PostEmbed.php
@@ -0,0 +1,21 @@
+<?php 
+	$faker = new Faker\Generator();
+
+	$factory->define(App\Embed::class, function(Faker\Generator $faker) {
+		$post = App\Post::find(rand(50000, 50390));
+		$user = App\User::find(rand(1, 22017));
+
+		// Import Internet Provider
+		$faker->addProvider(new Faker\Provider\Internet($faker));
+
+		return [
+			'id_post'    => (empty($post) ? 50390 : $post->id),
+			'accessurl'  => $faker->url(),
+			'domain'     => $faker->domainName(),
+			'shareid'    => (empty($user) ? $user->id : 22017),
+			'view'       => rand(1000, 200000),
+			'created_on' => date('Y-m-d H:i:s') 
+		];
+	});
+
+?>
